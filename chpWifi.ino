abf07094eeaacecff5e327826d4a3961f2e8ff32
chatpeth.ke <chatpeth.ke@inet.co.th>
2020-10-28 14:41:04 +0700

    ans

--- chpWifi.ino
+++ chpWifi.ino
@@ -4,9 +4,9 @@ void setup() {
   delay(1000);
   Serial.begin(115200);
   Serial.println();
-  esp_wifi_begin();
+  chp_wifi_begin();
 }
 
 void loop() {
-  esp_wifi_handle();
+  chp_wifi_handle();
 }
